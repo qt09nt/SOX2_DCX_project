f4db99ccab7ff39d54697ca8ba3c682cd496255e
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2022-01-07 17:32:35 -0500

    Add files via upload

--- secretome/organoid_secretome_perseus_working_matrix_sofia_volcano_plots.R
+++ secretome/organoid_secretome_perseus_working_matrix_sofia_volcano_plots.R
@@ -384,3 +384,185 @@ setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/
 plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641344672042.tsv',
                      gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641344672042.tsv', timepoint1 = "week 4", timepoint2 = "week 12")
 
+#week 4 vs week 15
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_4_vs_week_15.GseaPreranked.1641346821324")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641346821324.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641346821324.tsv',
+                     timepoint1 = "week 4", timepoint2 = "week 15")
+
+#week 6 vs week 8
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_6_vs_week_8.GseaPreranked.1641347608469")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641347608469.tsv',
+                    gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641347608469.tsv',
+                    timepoint1 = "week 6", timepoint2 = "week 8")
+
+#week 6 vs week 10
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_6_vs_week_10.GseaPreranked.1641348907004")
+#week 6 FDR q value cut off used= 0.1;
+#week 10 FDR q value cut off used= 0.25;
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641348907004.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641348907004.tsv',
+                     timepoint1 = "week 6", timepoint2 = "week 10")
+
+#week 6 vs week 12
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_6_vs_week_12.GseaPreranked.1641349594626")
+#week 6 FDR q value cut off used = 0.3
+#week 12 FDR q value cut off used = 0.4
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641349594626.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641349594626.tsv',
+                     timepoint1 = "week 6", timepoint2 = "week 12")
+
+#week 6 vs week 15
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_6_vs_week_15.GseaPreranked.1641350895168")
+#week 6 FDR q value cut off used = 0.15
+#week 15 FDR q value cut off used = 0.4
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641350895168.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641350895168.tsv',
+                     timepoint1 = "week 6", timepoint2 = "week 15")
+
+#week 8 vs week 10
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_8_Week_10.GseaPreranked.1641393881666")
+#week 8 FDR q value cut off used = 0.15
+#week 10 FDR q value cut off used = 0.25
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641393881666.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641393881666.tsv',
+                     timepoint1 = "week 8", timepoint2 = "week 10")
+
+
+#week 8 vs week 12
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_8_Week_12.GseaPreranked.1641394091767")
+#week 8 FDR q value cut off used = 0.15
+#week 10 FDR q value cut off used = 0.25
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641394091767.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641394091767.tsv',
+                     timepoint1 = "week 8", timepoint2 = "week 12")
+
+
+#week 8 vs week 15
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_8_Week_15.GseaPreranked.1641394576860")
+##week 8 FDR q value cut off used = 0.15
+#week 15 FDR q value cut off used = 0.25
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641394576860.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641394576860.tsv',
+                     timepoint1 = "week 8", timepoint2 = "week 15")
+
+#week 10 vs week 12 
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_10_Week_12.GseaPreranked.1641394862016")
+##week 10 FDR q value cut off used = 0.15
+#week 12 FDR q value cut off used = 0.25
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641394862016.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641394862016.tsv',
+                     timepoint1 = "week 10", timepoint2 = "week 12")
+
+#week 10 vs Week 15
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_10_Week_15.GseaPreranked.1641395126881")
+#week 10 FDR q value cut off = 0.15
+#week 15 FDR q value cut off = 0.25
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395126881.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395126881.tsv',
+                     timepoint1 = "week 10", timepoint2 = "week 15")
+
+###############week 16 vs Week 4
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_4.GseaPreranked.1641395284942")
+
+#gsea_report_for_na_neg had "---" values in the NES and NOM p-value columns for the first 3 rows so they were removed
+#(corresponding to GOMF_EXTRACELLULAR_MATRIX_STRUCTURAL_CONSTITUENT, GOBP_REGULATION_OF_MAPK_CASCADE, GOBP_POSITIVE_REGULATION_OF_MAPK_CASCADE
+#)pathways in order to try to fix this error: "Error: Discrete value supplied to continuous scale"
+
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395284942.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395284942_removed_missing.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 4")
+
+#timepoint 1 = week 16 
+week_16_vs_week_4_na_pos_gsea <-read.table(file = 'gsea_report_for_na_pos_1641395284942.tsv', sep = '\t', header = TRUE, fill = TRUE)
+colnames(week_16_vs_week_4_na_pos_gsea)[1]<-"Pathways"  #rename first column to Pathways 
+filtered_week_16 <- week_16_vs_week_4_na_pos_gsea[week_16_vs_week_4_na_pos_gsea$`NOM.p.val` <= 0.07, ]
+filtered_week_16[12]<-NULL
+filtered_week_16$timepoint <- "week 16"
+
+#timepoint 2 = week 4
+week_16_vs_week_4_na_neg = read.table(file = 'gsea_report_for_na_neg_1641395284942_removed_missing.tsv', sep = '\t', header = TRUE, fill = TRUE)
+colnames(week_16_vs_week_4_na_neg)[1]<-"Pathways"
+filtered_week_4 <- week_16_vs_week_4_na_neg[week_16_vs_week_4_na_neg$`NOM.p.val` <= 0.07, ]
+filtered_week_4$timepoint <- "week 4"
+
+#merge the two dataframes together:
+merged <- rbind(filtered_week_16, filtered_week_4)
+
+ggplot(merged, aes(x = timepoint, y = Pathways, size = NES, color = FDR.q.val )) +
+  geom_point(alpha = 0.7) + #scatterplot
+  scale_size(range = c(2, 5), name = "NES") +  #change the size of the points and create a name for the size legend
+  scale_color_viridis(discrete = FALSE)
+
+
+###############week 16 vs Week 6
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_6.GseaPreranked.1641395346683")
+#note for week 16 corresponds to postive NES while week 6 (timepoint 2 is negative NES)
+#week 16 `FDR.q.val` <= 0.7
+#week 6 `FDR.q.val` <= 0.5
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395346683.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395346683_no_missing_value.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 6")
+
+
+#week 16 vs week 8
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_8.GseaPreranked.1641395478737")
+#week 16 `FDR.q.val` <= 0.7
+#week 8 `FDR.q.val` <= 0.5
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395478737.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395478737_missing_removed.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 8")
+
+
+#week 16 vs week 10
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_10.GseaPreranked.1641395645362")
+#week 16 is gsea_na_pos; `FDR.q.val` <= 0.7
+#week 10 gsea_na_neg; `FDR.q.val` <= 0.7
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395645362.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395645362.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 10")
+
+
+#week 16 vs week 12
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_12.GseaPreranked.1641395689502")
+#week 16 is gseas_na_pos: `FDR.q.val` <= 0.5
+#week 12 is gsea_na_neg: `FDR.q.val` <= 0.7
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395689502.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395689502.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 12")
+
+#week 16 vs week 15
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_Week_16_Week_15.GseaPreranked.1641395866989")
+#week 16 is gseas_na_pos; `FDR.q.val` <= 0.3
+##week 12 is gsea_na_neg `FDR.q.val` <= 0.7
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641395866989.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641395866989.tsv',
+                     timepoint1 = "week 16", timepoint2 = "week 15")
+
+
+
+################trying to write a  loop to plot NES automatically 
+#but file names not consistently named enough to work
+
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types")
+path = ("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types")
+file_list <- list.files(path=path)
+
+for (i in file_list){
+  setwd(paste0("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/", i))
+  my_files = list.files()       #retrieve the file names in the GSEA pre-ranked results directory
+  #get the index number of the na_pos_report by searching for a file that begins with "gsea_report_for_na_pos"
+  #and ends with ".tsv"    (gsea report for positively enriched pathways, which correspond to the pathways enriched in timepoint 1)
+  #then also retrieve the index of the na_neg_report (pathways enriched in timepoint 2)
+  na_pos_report_index <- grep("^gsea_report_for_na_pos.*\\.tsv$", my_files)  
+  na_neg_report_index <- grep("^gsea_report_for_na_neg.*\\.tsv$", my_files)
+  #find the file which has the timepoint 1 and timepoint 2 information in the file name
+  comparison_info_file_index <- grep("*\\.rpt", my_files)
+  name<- toString(my_files[comparison_info_file_index])
+  #get name of timepoint 1:
+  timepoint1 <- substr(name, 16, 21)      #should retrieve the timepoint 1 from the comparison info file name ie. week_4
+  timepoint2 <- substr(name, 26, 32)       
+  
+  plot_gsea_timepoints(gsea_report_na_pos = my_files[na_pos_report_index], gsea_report_for_na_neg = my_files[na_neg_report_index])
+                       
+}
