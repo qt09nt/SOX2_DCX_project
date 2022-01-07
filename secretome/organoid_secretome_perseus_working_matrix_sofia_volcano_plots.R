4552f08c9b1373cbabf27f9f0cbd4171a302d140
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2022-01-07 10:37:07 -0500

    Add files via upload

--- secretome/organoid_secretome_perseus_working_matrix_sofia_volcano_plots.R
+++ secretome/organoid_secretome_perseus_working_matrix_sofia_volcano_plots.R
@@ -26,6 +26,8 @@ library(factoextra)
 library(DEGreport)
 library(dplyr)
 
+library(ggplot2)
+
 setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr")
 
 #load functions
@@ -328,4 +330,57 @@ pscore_table = get_pscore_table(SYN_week12_v_SYN_week15_top_table)
 write.table(pscore_table, paste("SYN", timepoint1, timepoint2, "pscore.rnk", sep = "_"),
             sep = "\t", col.names = F, row.names = F, quote = F)  
 
+
+##########Visualizing the GSEA results for timepoint comparisons (includes all cell types: SYN, SOX2 and DCX)
+
+#GSEA pre-ranked was conducted on the proteome secretome timepoint comparisons p rank score files
+#with databases searched in Human_AllPathways_June_01_2017_symbol.gmt, Hallmarks(ha.lla.v7.4.symbols.gmt), 
+#Kegg(c2.cp.kegg.v7.4.symbols.gmt), reactome(c2.cp.reactome.v7.4.gymbols.gmt), wikipathways(c2.cp.wikipathways.v7.4.symbols.gmt),
+#go bp(c5.go.bp.v7.4.symbols.gmt), go cc(c5.go.cc.v7.4.symbols.gmt), go mf(c5.go.mf.v7.4.symbols.gmt) databases
+
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_4_vs_week_6.GseaPreranked.1641334710091")
+
+#load in the gesea_report_for_na_pos (positive enrichment for week 4 compared to week 6):
+week_4_vs_week_6_na_pos_gsea <-read.table(file = 'gsea_report_for_na_pos_1641334710091.tsv', sep = '\t', header = TRUE)
+colnames(week_4_vs_week_6_na_pos_gsea)[1] <-"Pathways"     #rename first column of GSEA pos results to "Pathways"
+
+#filter the GSEA results dataframe to keep just the pathways which have a NOM p-value of less than 0.065 and a 
+#FDR q values less than 0.1
+filtered <- week_4_vs_week_6_na_pos_gsea[week_4_vs_week_6_na_pos_gsea$`NOM.p.val` <= 0.07 && week_4_vs_week_6_na_pos_gsea$`FDR.q.val` <= 0.09, ]
+filtered$timepoint <- "week 4" 
+
+#load in the gsea report for na_neg (negative enrichment for week 4 compared to week 6)
+week_4_vs_week_6_na_neg_gsea <- read.table(file = 'gsea_report_for_na_neg_1641334710091.tsv', sep = '\t', header = TRUE)
+colnames(week_4_vs_week_6_na_neg_gsea)[1] <- "Pathways"   ##rename first column of GSEA pos results to "Pathways"
+filtered_timepoint_2 <- week_4_vs_week_6_na_neg_gsea[week_4_vs_week_6_na_neg_gsea$`NOM.p.val` <= 0.07 && week_4_vs_week_6_na_neg_gsea$`FDR.q.val` <= 0.09, ]
+filtered_timepoint_2$timepoint <- "week 6"
+
+#merge the GSEA positive and negative dataframes together
+merged <- rbind(filtered, filtered_timepoint_2)
+
+ggplot(merged, aes(x = timepoint, y = Pathways, size = NES, color = FDR.q.val )) +
+  geom_point(alpha = 0.7) + #scatterplot
+  scale_size(range = c(2, 5), name = "NES") +  #change the size of the points and create a name for the size legend
+  scale_color_viridis(discrete = FALSE)
+
+#GSEA plot of Week 4 vs Week 6 comparisons
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types_week_4_vs_week_6.GseaPreranked.1640185266242")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1640185266242.tsv',  
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1640185266242.tsv',
+                     timepoint1 = "week 4", timepoint2 = "week 6")
+
+#GSEA plot of Week 4 vs Week 8 comparisons
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_4_vs_week_8.GseaPreranked.1641340848426")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641340848426.tsv',
+    gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641340848426.tsv', timepoint1 = "week 4", timepoint2 = "week 8")
+
+#week 4 vs week 10
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_4_vs_week_10.GseaPreranked.1641341772629")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641341772629.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641341772629.tsv', timepoint1 = "week 4", timepoint2 = "week 10")
+
+#week 4 vs week 12
+setwd("C:/Users/qt09n/Desktop/Technical Analyst I UHN May 4 2021/organoid group/Sofia/prot_org_secr/results/all cell types/GSEA/all_cell_types/all_cell_types_week_4_vs_week_12.GseaPreranked.1641344672042")
+plot_gsea_timepoints(gsea_report_na_pos = 'gsea_report_for_na_pos_1641344672042.tsv',
+                     gsea_report_for_na_neg = 'gsea_report_for_na_neg_1641344672042.tsv', timepoint1 = "week 4", timepoint2 = "week 12")
 
