a39457f5d43037b3f456cb43a23c21acc897100d
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-04-28 16:00:57 -0400

    Add files via upload

---
+++ rstudio_synthwave_theme.R
@@ -0,0 +1,4 @@
+Synthwave85 <- "https://raw.githubusercontent.com/jnolis/synthwave85/master/Synthwave85.rstheme"
+rstudioapi::addTheme(Synthwave85, apply = TRUE)
+
+#https://github.com/jnolis/synthwave85
